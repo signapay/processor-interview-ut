6bcb7a2846fe13fca1488e0c0e42d0a58b97e375
Shreya Kolekar <shreyakolekar.official@gmail.com>
2024-09-26 20:22:46 -0500

    Add basic security measures: CORS

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 
 // Middleware setup
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: 'http://localhost:3000', methods: ['GET', 'POST'] }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
