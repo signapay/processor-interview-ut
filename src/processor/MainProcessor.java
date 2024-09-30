105ab5d2325b18c3bc3f07b8623ef9beaf20b809
William M <wminchew20202@gmail.com>
2024-09-29 19:01:06 -0500

    Fixed transaction handling
    Transaction handling is working but needs to be refactored to reduce the amount of reused code.

--- src/processor/MainProcessor.java
+++ src/processor/MainProcessor.java
@@ -18,10 +18,10 @@ import java.util.HashMap;
  */
 public class MainProcessor {
 
-	private List<List<String>> data = new ArrayList<>(); 		// ArrayList for storing data from given CSV files for persistence
-	private List<List<String>> failedTransactions;				// ArrayList for tracking bad transactions
-	private HashMap<String, ArrayList<String>> chartedAccounts; // Hashmap for storing sorted accounts]
-	private HashMap<String, ArrayList<String>> accountsForCollections; // HashMap for accounts to be given to collections
+	private List<List<String>> data; 												// ArrayList for storing data from given CSV files for persistence
+	private List<List<String>> failedTransactions = new ArrayList<>();;				// ArrayList for tracking bad transactions
+	private HashMap<String, ArrayList<String>> chartedAccounts = new HashMap<>(); 	// Hashmap for storing sorted accounts
+	private HashMap<String, ArrayList<String>> accountsForCollections = new HashMap<>(); // HashMap for accounts to be given to collections
 	
 	/**
 	 * Takes a csvFile and adds it to the data ArrayList. Returns an int as confirmation.
@@ -30,10 +30,7 @@ public class MainProcessor {
 	 * @throws Exception if there is a failure reading the file
 	 */
 	public String readFile(File csvFile) throws Exception {
-		// Clear/Assign the failedTransactions, chartAccounts, and accountsForCollections every new file
-		failedTransactions = new ArrayList<>(); 
-		chartedAccounts = new HashMap<>();
-		accountsForCollections = new HashMap<>();
+		data = new ArrayList<List<String>>();
 		
 		String line = "";
 		
@@ -79,43 +76,15 @@ public class MainProcessor {
 	}
 	
 	/**
-	 * Uses the data ArrayList and charts it by card number, storing it in chartedAccounts
+	 * Wraps around transactionBasedOnType in order to have error handling and ability to add more methods for new transactions
 	 */
 	private void chartAccounts() {
-		
-		String card = ""; 		// Temp card variable to store card number
-		String name = ""; 		// Temp name variable used to store account name
-		Float amount = 0F;		// Temp amount variable to store transaction amount
 		ArrayList<String> tempArray = new ArrayList<String>();	// Temp arraylist for storing information
-		
 		
 		// Iterate over the data arraylist which contains all given CSV file data
 		for(List<String> d : data) {
 			try {
-				name = d.get(0); 						// Get the name of the card owner
-				card = d.get(1);						// Get the card number
-				amount = Float.parseFloat(d.get(2));	// Get the amount of the transaction
-				
-				// If our hashmap already contains the current card get their current balance, account names and add to the balance
-				if(chartedAccounts.containsKey(card)){
-					tempArray = new ArrayList<String>();
-					
-					amount += Float.parseFloat(chartedAccounts.get(card).get(0));
-					
-					tempArray = chartedAccounts.get(card);
-					tempArray.set(0, String.valueOf(amount));
-					if(!tempArray.contains(name))
-						tempArray.add(name); 
-					
-					chartedAccounts.put(card, tempArray);
-				}
-				else {	// If the card doesn't exist add it, the account name, and its balance to the hashmap
-					tempArray = new ArrayList<String>();
-					tempArray.add(Float.toString(amount));
-					tempArray.add(name);
-					
-					chartedAccounts.put(card, tempArray);
-				}
+				transactionBasedOnType(d, tempArray);
 			}
 			catch(Exception e) {
 				failedTransactions.add(d); // Store any failed transactions to be displayed later
@@ -132,6 +101,133 @@ public class MainProcessor {
 		
 		return chartedAccounts;
 		
+	}
+	
+	/**
+	 * Function that determines the type of transaction and does all the calculations
+	 * 
+	 * TODO - refactor and reduce amount of reused code. Manipulating data with transactions that already exist could be added into another method
+	 * 
+	 * @param d List of Strings that is meant to be in a row format (from data)
+	 * @param tempArray temporary arrayList used for storage and moving around data between transactions.
+	 */
+	private void transactionBasedOnType(List<String> d, ArrayList<String> tempArray) {
+		// Add proper error handling for the card, amount, and cardtarget (ensure they are numerical)
+		String name = d.get(0); 					// Get the name of the card owner
+		String card = d.get(1);						// Get the card number
+		Float amount = Float.parseFloat(d.get(2));	// Get the amount of the transaction
+		String type = d.get(3);						// Get the type of transaction
+		tempArray = new ArrayList<String>();
+		
+		// Switch that determines what type of transaction
+		switch(type) {
+			case "Transfer":
+				String cardTarget = d.get(5); // If the type is Transfer then we know there is a target card
+				// Check to see if these card already exist in the data base. We are assuming these transactions are real
+				if(chartedAccounts.containsKey(cardTarget) && chartedAccounts.containsKey(card)) {
+					amount += Float.parseFloat(chartedAccounts.get(cardTarget).get(0));
+					tempArray = chartedAccounts.get(cardTarget);
+					tempArray.set(0, String.valueOf(amount));
+					chartedAccounts.put(cardTarget, tempArray);
+					
+					// Rem
+
+					amount = Float.parseFloat(chartedAccounts.get(card).get(0)) - amount;
+					tempArray = chartedAccounts.get(card);
+					tempArray.set(0, String.valueOf(amount));
+					
+					if(!tempArray.contains(name))
+						tempArray.add(name); 
+					
+					chartedAccounts.put(card, tempArray);
+				}
+				else if (chartedAccounts.containsKey(cardTarget)){	// If the card doesn't exist add it, the account name, and its balance to the hashmap
+					amount = -amount;
+					tempArray.add(Float.toString(amount));
+					tempArray.add(name);
+					
+					chartedAccounts.put(card, tempArray);
+					
+					tempArray = new ArrayList<String>();
+					amount = -amount;
+					amount += Float.parseFloat(chartedAccounts.get(cardTarget).get(0));
+					tempArray = chartedAccounts.get(cardTarget);
+					tempArray.set(0, String.valueOf(amount));
+					chartedAccounts.put(cardTarget, tempArray);
+					
+				}
+				else if (chartedAccounts.containsKey(card)) { // If the target card doesn't exist add it, the account name, and its balance
+					amount = Float.parseFloat(chartedAccounts.get(card).get(0)) - amount;
+					tempArray = chartedAccounts.get(card);
+					tempArray.set(0, String.valueOf(amount));
+					
+					if(!tempArray.contains(name))
+						tempArray.add(name); 
+					
+					chartedAccounts.put(card, tempArray);
+					
+					tempArray = new ArrayList<String>();
+					amount = -amount;
+					tempArray.add(Float.toString(amount));
+					tempArray.add(name);
+					
+					chartedAccounts.put(cardTarget, tempArray);
+				}
+				else if (!chartedAccounts.containsKey(cardTarget) && !chartedAccounts.containsKey(card)){ // Assuming neither the target nor original exist
+					amount = -amount;
+					tempArray.add(Float.toString(amount));
+					tempArray.add(name);
+					
+					chartedAccounts.put(card, tempArray);
+					
+					tempArray = new ArrayList<String>();
+					amount = -amount;
+					tempArray.add(Float.toString(amount));
+					tempArray.add(name);
+					
+					chartedAccounts.put(cardTarget, tempArray);
+				}
+				break;
+			case "Credit":
+				if (chartedAccounts.containsKey(card)){
+					amount = Float.parseFloat(chartedAccounts.get(card).get(0)) - amount;
+					tempArray = chartedAccounts.get(card);
+					tempArray.set(0, String.valueOf(amount));
+					if(!tempArray.contains(name))
+						tempArray.add(name); 
+					
+					chartedAccounts.put(card, tempArray);
+				}
+				else {
+					amount = -amount;
+					tempArray.add(Float.toString(amount));
+					tempArray.add(name);
+					
+					chartedAccounts.put(card, tempArray);
+				}
+				break;
+			case "Debit":
+				if (chartedAccounts.containsKey(card)) {
+					amount = Float.parseFloat(chartedAccounts.get(card).get(0)) + amount;
+					tempArray = chartedAccounts.get(card);
+					tempArray.set(0, String.valueOf(amount));
+					
+					if(!tempArray.contains(name))
+						tempArray.add(name); 
+					
+					chartedAccounts.put(card, tempArray);
+				}
+				else {
+					tempArray.add(Float.toString(amount));
+					tempArray.add(name);
+					
+					chartedAccounts.put(card, tempArray);
+				}
+				break;
+			default:
+				// Add an error for no found type
+		}
+
 	}
 	
 	/**
